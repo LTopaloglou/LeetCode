eb18499091bb6b10a7ba1495452141430f2561c8
Leonidas Topaloglou Mundy <47695745+LTopaloglou@users.noreply.github.com>
2022-05-10 22:26:12 -0400

    Time: 19 ms (58.63%), Space: 10.8 MB (45.69%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,13 +3,13 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        int length = nums.size();
         unordered_map<int, int> table;
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < length; i++) {
             if (table.find(nums[i]) != table.end()) {
                 return {table[nums[i]], i};
-            } else {
-                table[target-nums[i]] = i;
             }
+            table[target-nums[i]] = i;
         }
         return {999, 999};
     }
